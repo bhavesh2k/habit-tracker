7facb6350b5c1a718bc03b5e3a6e517a5816b03a
bhavesh2k <bhavesh.agarwal2k@mail.com>
2025-02-10 12:22:45 +0530

    Add Firebase config and setup

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCnMU-CzSmD4d2AiS20bcIMrr1_eneft68",
+  authDomain: "habit-tracker-580d5.firebaseapp.com",
+  projectId: "habit-tracker-580d5",
+  storageBucket: "habit-tracker-580d5.firebasestorage.app",
+  messagingSenderId: "1088854789740",
+  appId: "1:1088854789740:web:80261dbf30bb078c89d249"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
